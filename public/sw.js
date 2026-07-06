832303aa122a82488f927a5d830252f1a5f99844
Kone <philipkone45@.com>
2026-07-06 05:26:27 +0000

    chore(pwa): optimize PWA manifest, service workers, and 512px icons

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'kone-warp-cache-v1';
+const STATIC_ASSETS = [
+  './logo512.png',
+  './favicon-192x192.png',
+  './favicon-48x48.png',
+  './logos/logo.svg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (STATIC_ASSETS.some(a => event.request.url.includes(a.replace('./', '')))) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
